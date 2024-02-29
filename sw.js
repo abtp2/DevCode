96860bffcc437313ce6c24d7f39eb4b7687e1357
Ashutosh Pandey <94055977+abtp2@users.noreply.github.com>
2024-03-01 02:44:47 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,31 @@
 self.addEventListener('install', function(e) {
     e.waitUntil(
-        caches.open('devcode').then(function(cache) {
+        caches.open('devcode-api').then(function(cache) {
             return cache.addAll([
                 '/',
                 'index.html',
                 'js/index.js',
                 'css/index.css',
                 'logo.png',
-                'pyicon.png'
+                'pyicon.png',
+                'ace/jquery.min.js',
+                'js/skulpt.min.js',
+                'js/skulpt-stdlib.js',
+                'js/jquery.min.js',
+                'icons/boxicons.css',
+                'js/ace.js',
+                'ace/theme-github_dark.js',
+                'ace/theme-twilight.js',
+                'ace/theme-one_dark.js',
+                'ace/theme-dracula.js',
+                'ace/theme-tomorrow_night.js',
+                'ace/theme-clouds_midnight.js',
+                'js/ext-language_tools.min.js',
+                'ace/ext-beautify.js',
+                'ace/mode-python.js',
+                'js/iro@5.js',
+                'js/index.js',
+                'js/editor.js'
             ]);
         })
     );
